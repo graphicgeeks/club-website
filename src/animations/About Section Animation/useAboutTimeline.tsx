114fcd0caf39b7390608723ea8de6069c58dcba0
gc_MayankPun <punmayank35@gmail.com>
2026-01-09 22:46:25 +0530

    feat: Implement animations for About, Intro, and Hero sections; refactor related components and styles

---
+++ src/animations/About Section Animation/useAboutTimeline.tsx
@@ -0,0 +1,56 @@
+import { splitWordsToSpans } from "@/utils/splitWordsToSpans";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import gsap from "gsap";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+const useAboutTimeline = () => {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top top",
+          // end: "top -20%", // accept if want automatic
+          end: "top -100%", // remove if want automatic
+          pin: true,
+          scrub: true, // remove if want automatic
+        },
+      });
+      tl.fromTo(
+        ".intro-head",
+        { opacity: 0, y: "5rem" },
+        {
+          opacity: 1,
+          y: 0,
+          ease: "cubic-bezier(0.68, -0.55, 0.27, 1.55)",
+        }
+      );
+
+      const paragraph = document.querySelector(".intro-text");
+      if (!paragraph) return;
+
+      splitWordsToSpans(paragraph);
+
+      tl.fromTo(
+        ".intro-text span",
+        { opacity: 0, y: "1rem" },
+        {
+          opacity: 1,
+          y: 0,
+          ease: "power3.inOut",
+          stagger: 0.1,
+        }
+      );
+    },
+    { scope: container }
+  );
+
+  return container;
+};
+
+export default useAboutTimeline;
---
+++ src/animations/Intro Section Animation/useIntroTimeline.tsx
@@ -0,0 +1,83 @@
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const useIntroTimeline = () => {
+  const container = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(
+    () => {
+      const cards = gsap.utils.toArray<HTMLElement>(".intro-card");
+
+      cards.forEach((card, index) => {
+        const textEls = card.querySelectorAll("h3, p");
+
+        // stacking order
+        gsap.set(card, { zIndex: index + 1 });
+
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: card,
+            start: "top top",
+            end: "+=100%",
+            pin: true,
+            pinSpacing: false,
+            scrub: true,
+          },
+        });
+
+        // card animation
+        tl.fromTo(
+          card,
+          {
+            opacity: 0,
+            scale: 1.1,
+          },
+          {
+            opacity: 1,
+            scale: 1,
+            ease: "power3.out",
+          }
+        );
+
+        // text animation 
+        tl.fromTo(
+          textEls,
+          {
+            opacity: 0,
+            y: 40,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            stagger: 0.2,
+            ease: "power2.out",
+          },
+          "-=0.3"
+        );
+
+        // Gives the last panel a smooth scroll exit
+        if (index === cards.length - 1) {
+          tl.to(
+            textEls,
+            {
+              y: -20,
+              opacity: 0,
+              stagger: 0.1,
+              ease: "power1.inOut",
+            },
+            "-=0.2"
+          );
+        }
+      });
+    },
+    { scope: container }
+  );
+
+  return container;
+};
+
+export default useIntroTimeline;
